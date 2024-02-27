b4189381ec8069267a2de9233fa359a6fb60983f
GGB <rlqja1018@gmail.com>
2024-02-27 13:02:05 +0900

    feat - 카메라 전환 구현
    Q/E 버튼 누를시 해당 방향에 맞는 카메라로 시점 전환
    
    시점 전환 시 전환이 완료 될 때 까지
    PlayerInput을 비활성화 해서 입력값을 받지 않음.

--- Assets/Scripts/Camera/CemeraController.cs
+++ Assets/Scripts/Camera/CemeraController.cs
@@ -1,7 +1,9 @@
 using Cinemachine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public enum CameraState
 {
@@ -12,14 +14,45 @@ public enum CameraState
 public class CemeraController : MonoBehaviour
 {
     [SerializeField] Transform target;
+    PlayerInput input;
 
+    CinemachineBrain brain;
     [SerializeField] CinemachineVirtualCamera[] vcams;
+    int currentCameraIndex;
+
+    bool isBlending = false;
+
     private void Awake()
     {
+        brain = GetComponent<CinemachineBrain>();
+
         GameObject vcamsPref = Instantiate(Resources.Load<GameObject>("Camera/VCams"));
 
         vcams = vcamsPref.GetComponentsInChildren<CinemachineVirtualCamera>();
         VCamsInit();
+
+        input = target.parent.gameObject.GetComponent<PlayerInput>();
+    }
+
+    private void Start()
+    {
+        input.PlayerActions.CameraMove.started += CameraMove_Started;
+    }
+
+    private void Update()
+    {
+        // 내가 정의한 bool값은 블렌드가 시작 되었을 때 true 로 변함
+        // brain.IsBlending 은 현재 블렌딩이 진행중인지를 나타내기 때문에 
+        // 둘중 하나만 쓴다면 조건이 계속 걸려서 쓸데없이 메소드를 계속 들어갈거라고 생각해서 이런식으로 조건을 걸었음
+        if (brain.IsBlending && !isBlending)
+        {
+            OnBlendStarted();
+        }
+        else if(!brain.IsBlending && isBlending)
+        {
+            OnBlendCompleted();
+        }
+
     }
 
     void VCamsInit()
@@ -32,5 +65,51 @@ public class CemeraController : MonoBehaviour
         }
 
         vcams[0].Priority = (int)CameraState.Active;
+        currentCameraIndex = 0;
+    }
+
+    public void CameraMove(int idx)
+    {
+        int temp = currentCameraIndex; // 카메라 우선도는 한번에 변경 하는게 나을것 같아서 일단 현재 인덱스 저장
+        currentCameraIndex += idx;
+
+        if (currentCameraIndex < 0)
+            currentCameraIndex = vcams.Length - 1;
+        else if (currentCameraIndex >= vcams.Length)
+            currentCameraIndex = 0;
+
+        VCamReset(vcams[temp]);
+        vcams[currentCameraIndex].Priority = (int)CameraState.Active;
+
+        // 여기서 일시적으로 PlayerInput을 끄고 블렌딩이 완료 되었을 때 다시 켜면
+        // 카메라 움직임이 있을 때 캐릭터의 움직임을 제한해서 대각선같은 이상한 움직임을 막을 수 있을것 같음.
+        //OnBlendStarted();
+    }
+
+    void VCamReset(CinemachineVirtualCamera vcam)
+    {
+        vcam.Priority = (int)CameraState.Default;
+    }
+
+    private void CameraMove_Started(InputAction.CallbackContext context)
+    {
+        Vector2 value = context.ReadValue<Vector2>();
+        CameraMove((int)value.x);
+    }
+
+    private void OnBlendStarted()
+    {
+        // 블렌딩이 시작 되었으니 PlayerInput 끄기
+        isBlending = true;
+        input.enabled = false;
+        Debug.Log("Blending Start");
+    }
+
+    private void OnBlendCompleted()
+    {
+        // 블렌딩이 완료 되었으니 PlayerInput 켜기
+        isBlending = false;
+        input.enabled = true;
+        Debug.Log("Blending Completed");
     }
 }
