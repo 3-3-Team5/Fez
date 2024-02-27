43343980bff5b546eb770b83acdb57131d161596
황문규 <155706789+GeCrabKani@users.noreply.github.com>
2024-02-27 22:24:44 +0900

    feat: 테스트 플레이어 스크립트 추가

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Inventory inventory = new Inventory(4);
+
+
+    public void RemoveItem(int index)
+    {
+        inventory.RemoveItem(index);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Puzzle"))
+        {
+            inventory.AddItem(other.GetComponent<ItemObject>().itemData);
+        }
+    }
+}
